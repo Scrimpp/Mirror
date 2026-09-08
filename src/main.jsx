4d188d568f5ba5cfb287522a0bb62f39d8fad139
Scrimpp <Dizzyvizsla@gmail.com>
2026-09-08 11:24:12 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,4 @@ import MirrorChat from "./MirrorChat.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MirrorChat />
-  </React.StrictMode>
-);
+
